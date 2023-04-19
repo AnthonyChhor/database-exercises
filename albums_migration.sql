3db005edb3fa2348a0b95f5c5bd519ce4ac3550b
Anthony Chhor <anthonychhor@Anthonys-MacBook-Pro.local>
2023-04-19 12:11:06 -0500

    seeder exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,10 +1,10 @@
 USE codeup_test_db;
 
-CREATE TABLE IF NOT EXISTS album (
+CREATE TABLE IF NOT EXISTS albums (
     id INT NOT NULL AUTO_INCREMENT,
     artist VARCHAR(50) NOT NULL,
     name VARCHAR(50) NOT NULL,
-    release_date DATE,
+    release_date INT NOT NULL,
     sales DOUBLE NOT NULL,
     genre VARCHAR(50) NOT NULL,
     PRIMARY KEY(id)
---
+++ albums_seeder.sql
@@ -0,0 +1,4 @@
+USE codeup_test_db;
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUE ('Led Zeppelin', 'Led Zeppelin IV', 1971, 30.4, 'Hard Rock'), ('Michael Jackson', 'Thriller', 1982, 51.2, 'Pop'), ('AC/DC', 'Back in Black', 1980, 30.1, 'Hard Rock'), ('Eagles', 'Their Greatest Hits', 1976, 41.2, 'Country Rock');
